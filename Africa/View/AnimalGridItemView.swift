57f9265797462145414ec211ce6a65a8b28a6cb1
John Michael Baldonado <jm.baldonado@whitecloak.com>
2022-07-04 23:45:54 +0800

    develop complex grid layout system with four toolbar buttons

---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,28 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by John Michael Baldonado on 7/4/22.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    let animal: Animal
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode(_file: "animals.json")
+    
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
