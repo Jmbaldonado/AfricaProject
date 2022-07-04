34aa569574fdbf4ff751a14630f702bee4b047cd
John Michael Baldonado <jm.baldonado@whitecloak.com>
2022-07-04 21:42:02 +0800

    develop complex map with custom animation

---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by John Michael Baldonado on 7/4/22.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+}
